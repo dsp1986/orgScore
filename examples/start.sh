a9bfa71c7f835b6aca262567a880e81b6ccefee2
Dominik Schmidt-Philipp <dsp@tonmaschine.de>
2016-01-18 01:08:10 +0100

    basic working example with 8 open pd instances

---
+++ examples/start.sh
@@ -0,0 +1,9 @@
+pd -nogui -jack -send "startup port 7000, name matrix, sends 7001 7002 7003 7004 7005 7006 7007" matrix.pd &
+
+pd -nogui -jack -send "startup port 7001, name time1, period 2000, pl 0" zz-orgscore.pd &
+pd -nogui -jack -send "startup port 7007, name time4, period 2000, pl 0, listen time1" zz-orgscore.pd &
+pd -nogui -jack -send "startup port 7002, name sf1, listen time4, sf bassdrum.wav" sf-orgscore.pd &
+pd -nogui -jack -send "startup port 7003, name time2, period 2000, pl 0 0.25 0.5 0.75, listen time1" zz-orgscore.pd &
+pd -nogui -jack -send "startup port 7004, name sf2, listen time2, sf hihat.wav" sf-orgscore.pd &
+pd -nogui -jack -send "startup port 7005, name time3, period 2000, pl 0.5, listen time1" zz-orgscore.pd &
+pd -nogui -jack -send "startup port 7006, name sf3, listen time3, sf snare.wav" sf-orgscore.pd &
